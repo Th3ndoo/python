01714d8c1efccfde4df08e2884dfc8f9d47001ae
Th3ndoo <lufunomakhado126@gmail.com>
2025-03-26 19:11:39 +0200

    Add files via upload

---
+++ afterclass.py
@@ -0,0 +1,13 @@
+# store birthdays
+
+friend1 = "12/11/2013"
+friend2 = "16/03/2010"
+friend3 = "02/12/2011"
+friend4 = "19/08/2010"
+friend5 = "04/03/2009"
+
+print(friend1,)
+print(friend2,)
+print(friend3,)
+print(friend4,)
+print(friend5,)
