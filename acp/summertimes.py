c62121bb2617e675e8c9d90c5f68c9b4417e1e32
Th3ndoo <lufunomakhado126@gmail.com>
2025-04-07 19:13:37 +0200

    Add files via upload

---
+++ acp/summertimes.py
@@ -0,0 +1,6 @@
+temperature = int(input("Get the temperature"))
+
+if temperature > 20:
+    print("it is preferable to wear summer clothes")
+else:
+    print("it is not preferable to wear summer clothes")
