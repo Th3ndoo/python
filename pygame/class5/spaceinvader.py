d91521de3fa7712cde6bbc4adb7b70d5eae4bc9e
Th3ndoo <lufunomakhado126@gmail.com>
2025-07-28 19:37:07 +0200

    Add files via upload

--- pygame/class5/spaceinvader.py
+++ pygame/class5/spaceinvader.py
@@ -17,11 +17,57 @@ COLLISION_DISTANCE = 27
 # Inititate Pygame
 pygame.init()
 
+
 # Create the screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Backround
 backround = pygame.image.load('backgroundspace.png')
+
+# Enemy
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+nun_of_enemies = 6
+
+for _i in range(nun_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0, SCREEN_WIDTH - 64))
+    enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
+    enemyX_change.append(ENEMY_SPEED_X)
+    enemyY_change.append(ENEMY_SPEED_Y)
+
+
+# Score
+score_value = 0
+font = pygame.font.Font("freesansbold.ttf", 32)
+textX = 10
+textY = 10
+
+# Game over text
+over_font = pygame.font.Font("freesansbold.ttf", 64)
+
+def show_score(x,y):
+    score = font.render("Score : "+ str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x,y))
+def game_over_text():
+    game_over_text = over_font.render("Game over", True  (255, 255, 255))
+    screen.blit(game_over_text, (200, 250))
+
+def player(x,y):
+    screen.blit(playerImg, (x, y))
+
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+    
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt((enemyX - bulletX) ** 2 + (enemyY - enemyX) ** 2)
+    return distance < COLLISION_DISTANCE
+
+
+
 
 # Player
 playerImg = pygame.image.load('player.png')
@@ -33,8 +79,9 @@ def player(x,y):
     # Draw the player on the screen
     screen.blit(playerImg, (x,y))
 
+
 # Bullet
-bulettImg = pygame.image.load('bullet.png')
+bulletImg = pygame.image.load('bullet.png')
 bulletX = 0
 bulletY = PLAYER_START_Y
 bulletX_change = 0
@@ -65,13 +112,31 @@ while running:
             playerX_change = 0 
     playerX += playerX_change
     playerX = max(0, min(playerX, SCREEN_WIDTH - 64))
+
+    # Enemy movement
+    for i in range(nun_of_enemies):
+        if enemyY[i] > 340: # Game Over Condition
+            for j in range(nun_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+        
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0 or enemyX[i] >= SCREEN_WIDTH - 64:
+            enemyX_change[i] *= -1 
+            enemyY[i] += enemyY_change[i]
+
+        
+    
     if bulletY<=0:
         bulletY = PLAYER_START_Y
         bullet_state = "ready"
     elif bullet_state == "fire":
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletX_change
-    
-    player(playerX, playerY)
 
+    enemy(enemyX[i], enemyY[i], -1)    
+
+    player(playerX, playerY)
+    show_score(textX,textY)
     pygame.display.update()
